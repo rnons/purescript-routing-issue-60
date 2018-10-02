7777f279ff3d496cea544483576b4908c09eaea6
Ping Chen <remotenonsense@gmail.com>
2018-10-02 12:30:00 +0900

    pin to new purescript-routing-duplex commit

--- src/Main.purs
+++ src/Main.purs
@@ -3,8 +3,10 @@ module Main where
 import Prelude
 
 import App (Query(..), app)
+import Data.Either (Either(..))
+import Debug.Trace (trace)
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff (error, launchAff_)
 import Halogen as H
 import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.VDom.Driver (runUI)
@@ -19,5 +21,7 @@ main = do
     body <- awaitBody
     app' <- runUI app unit body
 
-    H.liftEffect $ router # matchesWith (RD.parse pageRoute) \_ route ->
+    H.liftEffect $ router # matchesWith (\s -> case RD.parse pageRoute s of
+      Left err -> trace err $ \_ -> Left err
+      Right r -> Right r) \_ route ->
       launchAff_ $ app'.query (H.action $ RouteChange route)
